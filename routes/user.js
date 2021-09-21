9848a222ec16dbc711771961bd5dbe47627529e9
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-09-21 10:26:20 +0300

    Updated the user routes

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,53 @@
 const express = require("express")
 const router = express.Router()
 const User = require('../classes/user')
-const verifyToken = require('../middleware/auth');
+// const verifyToken = require('../middleware/auth');
+const bcrypt = require('bcrypt');
+// const gentoken = require('../helpers/generateToken');
+const Joi = require('joi');
 
-// const {create, getOne, deleteUser, loginUser, updateUser}  = require('../controllers/usersControllers')
 const {login, register, deleteUser}  = require('../contol/userController')
 
-// router.post('/', create)
 router.get('/login', (req, res) =>{
+    const schema = Joi.object().keys({
+        username: Joi.string().min(6).required(),
+        password: Joi.string().min(8).required()
+    })
+    const {error} = schema.validate(req.body)
+    if(error){
+        return res.status(400).send({message: error.details[0].message});
+    }
+
+
     login(req.body.username,
     req.body.password
     )
 })
+
 router.post('/register', (req, res) =>{
+    const schema = Joi.object().keys({
+        username: Joi.string().min(6).required(),
+        password: Joi.string().min(8).required(),
+        project : Joi.string().min(5).required()
+    })
+
+    const {error} = schema.validate(req.body)
+    if(error){
+        return res.status(400).send({message: error.details[0].message});
+    }
     register(
         req.body.username,
         req.body.password,
         req.body.project,
     )
+    
+    
 })
 
 router.delete('/', (req, res)=>{
     deleteUser( req.body.username)
-})
+})  
+
 // router.put('/', updateUser)
 
 module.exports = router;
